93c73f782d6a94c6874021c5d50ae0ac3b4e7285
unknown <draknight2000@gmail.com>
2024-08-22 15:20:47 +0700

    Creat base project

---
+++ src/components/login/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = (props) => {
+    return (
+        <div className='login-container'>
+            login form
+            <button> Test</button>
+        </div>
+    );
+}
+
+export default Login;
